7e55a62e3fb1891a63f9b1a7c26cf634cb8b014f
Commander147 <jafework@gmail.com>
2012-12-02 19:20:10 -0800

    Update TestGame/TestGame/TaskManager/TaskManager.cs
    Updated task manager

--- TestGame/TestGame/TaskManager/TaskManager.cs
+++ TestGame/TestGame/TaskManager/TaskManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+
 using System.Collections;
 using System.Linq;
 using System.Text;
@@ -14,67 +15,39 @@ namespace TestGame
 {
     class TaskManager
     {
-
-        private ArrayList[] tasks = new ArrayList[3];
-        private int total_size;
-        private Sprite location;
+        private ArrayList TaskQueue = new ArrayList();
 
         public TaskManager()
         {
-            Console.WriteLine(tasks);   
         }
 
         public bool taskIsEmpty()
         {
-            return (tasks[0].Count == 0 && tasks[1].Count == 0 && tasks[2].Count == 0);
+            return (TaskQueue.Count == 0 );
         }
 
-        public void taskAdd(int rank, Task task)
+        public void AddTask(Task newTask)
         {
-            for (int i = 0; i < tasks[rank].Count; i++)
+            TaskQueue.Add(newTask);
+        }
+
+        public void RemoveTask()
+        {
+            if (taskIsEmpty() == false)
             {
-                if (tasks[rank][i].internalPriorty <= task.internalPriorty)
-                {
-                    tasks[rank].Insert(i, task);
-                    break;
-                }
+                TaskQueue.Remove(0);
             }
         }
 
-        public ArrayList getTask()
+        public Task GetTask()
         {
-            bool foundWaiting = false;
-            List<int> destinationList = new List<int>();
-            int rank = 0;
-            for (int i = 0; i < tasks.Count(); i++)
+            if (taskIsEmpty() == false)
+                return (Task)TaskQueue[0];
+            else
             {
-                if (foundWaiting == false)
-                {
-                    if (tasks[rank][i].currentStatus == Task.taskStatus.waiting)
-                    {
-                        foundWaiting = true;
-                        destinationList.Add(rank);
-                        destinationList.Add(i);
-                    }
-                    if (tasks[rank][i].currentStatus == Task.taskStatus.executing)
-                    {
-                        destinationList.Add(rank);
-                        destinationList.Add(i);
-                        break;
-                    }
-                    rank++;
-                }
+                return null;
             }
-            return destinationList;
         }
-
-        //public void taskRemove(int rank, Task task)
-        //{
-
-        //}
-
-        //public void getTaskforExe() { }
-
     }
 
 }
