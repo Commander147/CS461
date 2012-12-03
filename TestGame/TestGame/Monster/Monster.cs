f53e83bbc76a46b36d04d9935c328606d0ef8829
Andrew Lee <mkrlsh635@gmail.com>
2012-12-02 16:04:04 -0800

    Vector2d problem

--- TestGame/TestGame/Monster/Monster.cs
+++ TestGame/TestGame/Monster/Monster.cs
@@ -1,47 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Collections;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace TestGame
 {
     
-    public class MonsterStats{
-        
-        public int hunger {
-            set {hunger = value;}
-            get {return hunger;}
-        };
-        public int thirst{
-            set {thirst = value;}
-            get {return thirst;}
-        };
-        public int exp{
-            set {exp = value;}
-            get {return exp;}
-        };
-        public int hp{
-            set {hp = value;}
-            get {return hp;}
-        };
-        public string name{
-            set {name = value;}
-            get {return name;}
-        };
-        
-        public MonsterStats(int hung,int t, int xp,int hp, string name){
-            this.hunger = hung;
-            this.thirst = t;
-            this.exp = xp;
-            this.hp = hp;
-            this.name = name;
-        }
-    }
-    
-    
     class Monster
     {
+        Vector2 location;
+        ArrayList location_log = new ArrayList[location];
         MonsterStatus Status;
 
         public Monster(int hung,int t, int xp,int hp, string name)
@@ -49,20 +24,15 @@ namespace TestGame
              Status = new MonsterStatus(hung,t, xp,hp,name);     
              TaskManager Tasks = new TaskManager();
         }
-        public ArrayList Status_Add
-        {
-           ArrayList status = new ArrayList();
-
-        }
         public bool IsAlive
         {
-            get { return status.hp > 0; }
+            get { return Status.hp > 0; }
         }
-
-         //public void poop()
-        //{
-        //    if (Monster.hunger >= .9 && 
-        //}
+        public void poop()
+        {
+            if (Status.hunger >= 80 && Status.thirst >= 60)
+                location_log 
+        }
 
 
         //public void Update(TaskManager task)
